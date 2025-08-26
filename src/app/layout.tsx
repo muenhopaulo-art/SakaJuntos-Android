b8435e08f6f8789b310066b6da04fa149b38ed38
Paulo Muenho <paulomu834@gmail.com>
2025-08-26 15:48:32 +0000

    o app esta muito lento

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,17 @@
 import type { Metadata } from 'next';
+import { PT_Sans } from 'next/font/google';
 import './globals.css';
 import { Toaster } from '@/components/ui/toaster';
 import { CartProvider } from '@/contexts/cart-context';
 import { SiteHeader } from '@/components/site-header';
 import { SiteFooter } from '@/components/site-footer';
+
+const ptSans = PT_Sans({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+  style: ['normal', 'italic'],
+  variable: '--font-pt-sans',
+});
 
 export const metadata: Metadata = {
   title: 'SakaJuntos Web',
@@ -16,15 +24,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="light">
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap"
-          rel="stylesheet"
-        />
-      </head>
+    <html lang="en" className={`light ${ptSans.variable}`}>
       <body className="font-body antialiased">
         <CartProvider>
           <div className="relative flex min-h-screen flex-col">
