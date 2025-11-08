d5c3b58ef3e79b8b5917250276b5da5f947d92a0
Paulo Muenho <paulomu834@gmail.com>
2025-11-08 15:53:57 +0000

    Não copia nenhuma informação eu quero que a aba do produtos em destaque

--- src/components/products-carousel.tsx
+++ src/components/products-carousel.tsx
@@ -1,4 +1,3 @@
-
 'use client';
 
 import { ProductCard } from '@/components/product-card';
@@ -25,15 +24,15 @@ export function ProductsCarousel({ products }: ProductsCarouselProps) {
             >
             <CarouselContent className="-ml-4">
                 {products.map(product => (
-                <CarouselItem key={product.id} className="sm:basis-1/2 md:basis-1/3 lg:basis-1/4 pl-4">
+                <CarouselItem key={product.id} className="basis-1/2 md:basis-1/3 lg:basis-1/4 pl-4">
                     <div className="p-1 h-full">
                     <ProductCard product={product} />
                     </div>
                 </CarouselItem>
                 ))}
             </CarouselContent>
-            <CarouselPrevious className="hidden md:flex" />
-            <CarouselNext className="hidden md:flex" />
+            <CarouselPrevious className="flex" />
+            <CarouselNext className="flex" />
         </Carousel>
     )
 }
