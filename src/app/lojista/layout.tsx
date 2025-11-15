ce9fbab955fd09cb34a2e3d724bfe31f93fb0653
Paulo Muenho <paulomu834@gmail.com>
2025-11-15 15:37:43 +0000

    olha onde esta apontar o erro

--- src/app/lojista/layout.tsx
+++ src/app/lojista/layout.tsx
@@ -10,11 +10,11 @@ export default function LojistaLayout({
 }) {
 
   return (
-    <div className="flex min-h-screen w-full flex-col bg-muted/40">
+    <>
       <LojistaHeader />
-      <main className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-8">
+      <main className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-8 bg-muted/40">
         {children}
       </main>
-    </div>
+    </>
   );
 }
