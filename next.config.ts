cc02eef825b9f6094e7589b4d9369a738362d99b
Paulo Muenho <paulomu834@gmail.com>
2025-11-15 13:29:42 +0000

    este erro que esta na imagem eu tenho esta sugestão O Diagnóstico (Corre

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,12 @@
 
+const withPWA = require('@ducanh2912/next-pwa')({
+  dest: 'public',
+  skipWaiting: true,
+  clientsClaim: true,
+  disable: process.env.NODE_ENV === 'development',
+  register: true,
+});
+
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
@@ -33,4 +41,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+export default withPWA(nextConfig);
